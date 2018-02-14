9b0ac9f643d105169951bf3f1363ce43121a5cab
Max Page <maxpage@Maxs-MacBook-Pro.local>
2018-02-14 14:54:50 -0500

    Completed Part I of project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/searchBar/searchBar';
+import BusinessList from './components/businessList/businessList';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
       </div>
     );
   }
